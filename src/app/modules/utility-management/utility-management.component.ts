2295232d4e7d043699bbdf5314b96bd28e8d91be
Antonio <web.antonio.popa@gmail.com>
2021-01-26 19:15:25 +0100

    Improved some feature in utility management

--- src/app/modules/utility-management/utility-management.component.ts
+++ src/app/modules/utility-management/utility-management.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Provider } from './interfaces/provider';
+import * as data from './data.json';
 
 @Component({
   selector: 'app-utility-management',
@@ -9,38 +10,15 @@ import { Provider } from './interfaces/provider';
 })
 export class UtilityManagementComponent implements OnInit {
 
-  public data: Provider[] = [
-    {
-      id: '1',
-      image: './assets/img/acea-logo.png',
-      title: 'Acea',
-      serviciu: 'Apa',
-      pret: '13',
-      data: '12/08/20'
-    },
-    {
-      id: '2',
-      image: './assets/img/engie-logo.png',
-      title: 'Engie',
-      serviciu: 'Curent',
-      pret: '15',
-      data: '25/10/20'
-    },
-    {
-      id: '3',
-      image: './assets/img/engie-logo.png',
-      title: 'Engie',
-      serviciu: 'Gaz',
-      pret: '12',
-      data: '06/07/20'
-    },
-  ];
+  public data: Provider[] = [];
 
   public priceForm = this.fb.group({
     price: [null, Validators.required]
   });
 
-  public currentElement = null;
+  public newData: Provider[] = [];
+
+  public elementSelectedToEdit = null;
 
   public actualCategory = 'All';
 
@@ -49,6 +27,8 @@ export class UtilityManagementComponent implements OnInit {
   public constructor(private fb: FormBuilder) { }
 
   public ngOnInit(): void {
+    this.data = (data  as  any).default;
+    this.newData = this.data;
   }
 
   public getUnitOfMisurement(typeOfService: string): string {
@@ -59,8 +39,13 @@ export class UtilityManagementComponent implements OnInit {
     }
   }
 
-  public filterData(filterType: string): Provider[] {
-    return (this.actualCategory === 'All') ? this.data : this.data.filter(element => element.serviciu === filterType);
+  public filterData(filterType: string): void {
+    this.actualCategory = filterType;
+    this.newData = (filterType === 'All') ? this.data : this.data.filter(element => element.serviciu === filterType);
+  }
+
+  public filterCategory(filterType: string): number {
+    return this.data.filter(element => element.serviciu === filterType).length;
   }
 
   public onStateChangeModal(): void {
@@ -68,7 +53,7 @@ export class UtilityManagementComponent implements OnInit {
   }
 
   public editPrice(id: string): void {
-    this.currentElement = id;
+    this.elementSelectedToEdit = id;
     this.onStateChangeModal();
   }
 
@@ -76,10 +61,17 @@ export class UtilityManagementComponent implements OnInit {
     return this.priceForm;
   }
 
+  public searchByProviderName(providerName: any): void {
+    this.newData = this.data.filter(element => element.title.toLowerCase().startsWith(providerName.target.value.toLowerCase()));
+    if (providerName.target.value === '') {
+      this.actualCategory = 'All';
+    }
+  }
+
   public onSubmit(): void {
     if (this.formPrice.valid) {
       this.data.map(element => {
-        if (element.id === this.currentElement) {
+        if (element.id === this.elementSelectedToEdit) {
           element.pret = this.formPrice.get('price').value;
           const today = new Date();
           const dd = today.getDate();
@@ -88,15 +80,10 @@ export class UtilityManagementComponent implements OnInit {
           element.data = `${dd}/${mm}/${yy}`;
         }
       });
+      this.formPrice.controls.price.setValue(null); // refresh the input to initial state
       this.onStateChangeModal();
     }
   }
-
 }
 
-
-/*
- * TODO:
- * 1. Sa golesc inputul cand il folosesc, findca cand editex alt box ramane pe input acel numar de dinainte
- * 2. Sa rezolv filtrarea ca sa se schimbe in mod reactiv in functie de ce categorie aleg
-*/
+// TODO: Some optimization in this code, like optimize the search option, because startWith is not actually good
